b468300f4f5921e3f3408e793916fb685d97435c
joestack <joern@joern-online.de>
2021-07-08 14:44:58 +0200

    3

--- server.tf
+++ server.tf
@@ -1,10 +1,10 @@
 
 data "template_file" "vault_server" {
   count = var.server_count
-  template = "${join("\n", tolist(
+  template = "${join("\n", tolist([
     file("${path.root}/templates/base.sh"),
     file("${path.root}/templates/server.sh")
-  ))}"
+  ]))}"
   vars = {
     server_count        = var.server_count
     #data_dir            = var.data_dir
