7d82dcf46b7cbf64328fea4e162b0c82931e2089
joestack <joern@joern-online.de>
2021-07-09 12:47:33 +0200

    c1

--- templates/server.sh
+++ templates/server.sh
@@ -167,35 +167,6 @@ Description=Consul Online
 RefuseManualStart=true
 EOF
 
-# cat <<EOF > ${systemd_dir}/consul-online.sh
-
-# #!/bin/bash
-
-# set -e
-# set -o pipefail
-
-
-# # waitForConsulToBeAvailable loops until the local Consul agent returns a 200
-# # response at the /v1/operator/raft/configuration endpoint.
-# #
-# # Parameters:
-# #     None
-# function waitForConsulToBeAvailable() {
-#   local consul_http_addr=$1
-#   local consul_leader_http_code
-
-#   consul_leader_http_code=$(curl --silent --output /dev/null --write-out "%{http_code}" "${consul_http_addr}/v1/operator/raft/configuration") || consul_leader_http_code=""
-
-#   while [ "x${consul_leader_http_code}" != "x200" ] ; do
-#     echo "Waiting for Consul to get a leader..."
-#     sleep 5
-#     consul_leader_http_code=$(curl --silent --output /dev/null --write-out "%{http_code}" "${consul_http_addr}/v1/operator/raft/configuration") || consul_leader_http_code=""
-#   done
-# }
-
-# waitForConsulToBeAvailable "${consul_http_addr}"
-# EOF
-
 systemctl enable consul
 systemctl start consul
 systemctl enable vault
