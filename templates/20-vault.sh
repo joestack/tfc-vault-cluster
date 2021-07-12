6c5a92de8acd9bb5e34804ac1af515aa9a6205bb
joestack <joern@joern-online.de>
2021-07-12 12:43:38 +0200

    d7

--- templates/20-vault.sh
+++ templates/20-vault.sh
@@ -16,19 +16,19 @@ listener "tcp" {
     #tls_key_file = "/vault/certs/vault_key.key"
 }
 
-# storage "raft" {
-#     path = "/opt/vault/data"
-
-#     node_id = "${node_name}"
-
-#     retry_join {
-#         leader_api_addr = "https://${node_name}:8200"
-#     }
-# }
-
-storage "file" {
+storage "raft" {
     path = "/opt/vault/data"
+
+    node_id = "${node_name}"
+
+    retry_join {
+        leader_api_addr = "https://${node_name}:8200"
+    }
 }
+
+# storage "file" {
+#     path = "/opt/vault/data"
+# }
 
 ui = true
 
